20c919fdf77e9e8da33d25975c26e815156f3bcc
Otavio Samenezes <otaviogomessamenezes@gmail.com>
2025-08-21 17:37:07 -0300

    Resolução

---
+++ src/Exo5.java
@@ -0,0 +1,39 @@
+//import javax.swing.*;
+//import java.awt.*;
+//public class Exo5 {
+   // public static void main(String[] args) {
+      //  JFrame janela = new JFrame("Resolução");
+        //janela.setSize(400, 400);
+        //janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //janela.setLayout(null);
+
+        //ImageIcon icone = new ImageIcon("src/imagem.jpg");
+        //JLabel labelImagem = new JLabel(icone);
+        //labelImagem.setBounds(0, 0, 400, 300);
+
+        //JButton botao = new JButton("Clicar aqui");
+        //int btnLargura = 120;
+        //int btnAltura = 30;
+
+        //int btnX = (labelImagem.getWidth() - btnLargura) / 2;
+        //int btnY = (labelImagem.getHeight() - btnAltura) / 2;
+        //botao.setBounds(btnX, btnY, btnLargura, btnAltura);
+
+        //JLabel labelTexto = new JLabel(" ", JLabel.CENTER);
+        //labelTexto.setBounds(0, 165, 400, 30);
+
+       // botao.addActionListener(e -> {
+          //  Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
+          //  int largura = tela.width;
+            //int altura = tela.height;
+
+    //        labelTexto.setText("Resolução: " + largura + " X " + altura);
+
+       // });
+        //janela.add(botao);
+        //janela.add(labelTexto);
+        //janela.add(labelImagem);
+
+        //janela.setVisible(true);
+   // }
+//}
