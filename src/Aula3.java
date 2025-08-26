a92bbeff4cc60f7323cffe62dc7050a56ae9be60
Otavio Samenezes <otaviogomessamenezes@gmail.com>
2025-08-26 15:31:22 -0300

    caixa de entrada

---
+++ src/Aula3.java
@@ -0,0 +1,7 @@
+// Saida simples usando println.
+public class Aula3 {
+    public static void main(String[] args) {
+       float nota = 9.7f;
+        System.out.println("Sua nota é " + nota);
+    }
+}
